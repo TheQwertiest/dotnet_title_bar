4e187f931ad46b15bbe5cbb3208d852330e174cf
Roman Plášil <Quiark@users.noreply.github.com>
2007-04-29 20:50:36 +0000

    update

--- fooTitle/AssemblyInfo.cs
+++ fooTitle/AssemblyInfo.cs
@@ -45,8 +45,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.6.99.*")]
-[assembly: AssemblyInformationalVersion("0.6.99")]
+[assembly: AssemblyVersion("0.7.0.*")]
+[assembly: AssemblyInformationalVersion("0.7.0")]
 
 /* Changelog:
    0.7
