ad12ad60e04a9cb739231826e19b216c88002f69
LepkoQQ <lepko.san@gmail.com>
2016-09-18 17:49:51 +0200

    Monitor edge snapping now works with multiple monitors and on right/bottom as well

--- fooTitle/Display.cs
+++ fooTitle/Display.cs
@@ -19,24 +19,20 @@
 */
 using System;
 using System.Drawing;
-using System.Collections;
-using System.ComponentModel;
 using System.Windows.Forms;
 
 using fooTitle.Config;
 
-namespace fooTitle
-{
-    public class Display : PerPixelAlphaForm
-	{
-		private System.ComponentModel.Container components = null;
-		private System.Drawing.Bitmap canvasBitmap = null;
-		public System.Drawing.Graphics Canvas = null;
+namespace fooTitle {
+    public class Display : PerPixelAlphaForm {
+        private System.ComponentModel.Container components = null;
+        private System.Drawing.Bitmap canvasBitmap = null;
+        public System.Drawing.Graphics Canvas = null;
 
         public int SnapDist = 10;
 
-		private bool dragging = false;
-		private int dragX;
+        private bool dragging = false;
+        private int dragX;
         private int dragY;
         private int opacity;
 
@@ -60,7 +56,7 @@ namespace fooTitle
 
             public int GetValue() {
                 long now = System.DateTime.Now.Ticks / 10000;
-                
+
                 // special cases
                 if (now == startTime)
                     return startVal;
@@ -99,17 +95,16 @@ namespace fooTitle
         /// </summary>
         public ConfEnum<Win32.WindowPosition> WindowPosition = ConfValuesManager.CreateEnumValue<Win32.WindowPosition>("display/windowPosition", Win32.WindowPosition.Topmost);
 
-		public Display(int width, int height)
-		{
-			InitializeComponent();
+        public Display(int width, int height) {
+            InitializeComponent();
 
-			canvasBitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            canvasBitmap = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
             Canvas = Graphics.FromImage(canvasBitmap);
             Canvas.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
 
-			this.Size = new Size(width, height);
-			this.Left = 250;
-			this.Top = 0;
+            this.Size = new Size(width, height);
+            this.Left = 250;
+            this.Top = 0;
 
             opacity = normalOpacity.Value;
 
@@ -117,75 +112,68 @@ namespace fooTitle
             windowPositionChangeDelegate = new ConfValuesManager.ValueChangedDelegate(windowPosition_OnChanged);
             normalOpacity.OnChanged += normalOpacityChangeDelegate;
             WindowPosition.OnChanged += windowPositionChangeDelegate;
-            
-            SetWindowsPos(WindowPosition.Value);
 
-		}
+            SetWindowsPos(WindowPosition.Value);
+        }
 
         void windowPosition_OnChanged(string name) {
-           SetWindowsPos(WindowPosition.Value);
+            SetWindowsPos(WindowPosition.Value);
         }
 
         private void normalOpacity_OnChanged(string name) {
             SetNormalOpacity(normalOpacity.Value);
         }
 
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
-		protected override void Dispose( bool disposing )
-		{
-			if( disposing )
-			{
-				if(components != null)
-				{
-					components.Dispose();
-				}
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
+                if (components != null) {
+                    components.Dispose();
+                }
 
                 // need to remove this from the events on the configuration values
                 normalOpacity.OnChanged -= normalOpacityChangeDelegate;
                 WindowPosition.OnChanged -= windowPositionChangeDelegate;
+            }
+            base.Dispose(disposing);
+        }
 
-			}
-			base.Dispose( disposing );
-		}
-
-		#region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			// 
-			// Display
-			// 
-			this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
-			this.ClientSize = new System.Drawing.Size(480, 96);
-			this.ControlBox = false;
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "Display";
-			this.ShowInTaskbar = false;
-			this.Text = "foo_title";
+        #region Windows Form Designer generated code
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            // 
+            // Display
+            // 
+            this.AutoScaleBaseSize = new System.Drawing.Size(5, 13);
+            this.ClientSize = new System.Drawing.Size(480, 96);
+            this.ControlBox = false;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Display";
+            this.ShowInTaskbar = false;
+            this.Text = "foo_title";
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Display_MouseDown);
-			this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Display_MouseUp);
-			this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Display_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Display_MouseUp);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Display_MouseMove);
             this.MouseEnter += new EventHandler(Display_MouseEnter);
             this.MouseLeave += new EventHandler(Display_MouseLeave);
             this.Activated += new EventHandler(Display_Activated);
-		}
+        }
+        #endregion
 
-		#endregion
-
-		public void FrameRedraw() {
+        public void FrameRedraw() {
             frameUpdateOpacity();
             SetBitmap(canvasBitmap, (byte)opacity);
             Canvas.Clear(Color.Transparent);
         }
-  
-#region Bottom
+
+        #region Bottom
         void Display_Activated(object sender, EventArgs e) {
             if (WindowPosition.Value == Win32.WindowPosition.Bottom) {
                 SetWindowsPos(Win32.WindowPosition.Bottom);
@@ -209,47 +197,50 @@ namespace fooTitle
             base.WndProc(ref m);
         }
 #pragma warning restore 0168, 219, 67
-#endregion 
-
+        #endregion
 
         #region Dragging
         private void Display_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e) {
-			dragging = true;
-			dragX = e.X;
+            dragging = true;
+            dragX = e.X;
             dragY = e.Y;
-		}
+        }
 
-		private void Display_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e) {
-			dragging = false;
+        private void Display_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e) {
+            dragging = false;
 
             // save position
             Main.GetInstance().SavePosition();
-		}
+        }
 
-		private void Display_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
+        private void Display_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e) {
             if (dragging) {
                 Point mouse = this.PointToScreen(new Point(e.X, e.Y));
+                Screen screen = Screen.FromPoint(mouse);
 
-                Left = doSnapping(mouse.X - dragX, 0);
-                Top = doSnapping(mouse.Y - dragY, 0);
+                this.Left = doSnapping(mouse.X - dragX, this.Width, screen.WorkingArea.Left, screen.WorkingArea.Right);
+                this.Top = doSnapping(mouse.Y - dragY, this.Height, screen.WorkingArea.Top, screen.WorkingArea.Bottom);
             }
-		}
+        }
 
         // snapping
-        private int doSnapping(int pos, int border) {
-            if (pos - border < SnapDist) {
-                if (Math.Abs(pos - border) > 2 * SnapDist) {
-                    return pos;
-                } else {
-                    return border;
+        private int doSnapping(int pos, int size, int border, int oppositeBorder) {
+            if ((Control.ModifierKeys & Keys.Control) == 0) {
+                int borderDist = Math.Abs(pos - border);
+                int oppositeBorderDist = Math.Abs(pos + size - oppositeBorder);
+                if (borderDist < SnapDist || oppositeBorderDist < SnapDist) {
+                    if (borderDist < oppositeBorderDist) {
+                        return border;
+                    } else {
+                        return oppositeBorder - size;
+                    }
                 }
             }
             return pos;
         }
-		#endregion
+        #endregion
 
         #region Opacity
-
         protected void frameUpdateOpacity() {
             if (opacityFade != null) {
                 opacity = opacityFade.GetValue();
@@ -271,7 +262,6 @@ namespace fooTitle
             opacityFade = null;
         }
         #endregion
-
 
         internal void SetSize(int width, int height) {
             this.Width = width;
