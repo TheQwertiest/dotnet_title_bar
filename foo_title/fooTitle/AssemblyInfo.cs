a734aedad4c3336ba7ed0f998983dd584969a4c8
TheQwertiest <qwertiest@mail.ru>
2017-08-01 13:47:52 +0300

    Version up

--- foo_title/fooTitle/AssemblyInfo.cs
+++ foo_title/fooTitle/AssemblyInfo.cs
@@ -45,8 +45,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.9.2.*")]
-[assembly: AssemblyInformationalVersion("0.9.2")]
+[assembly: AssemblyVersion("0.9.3.*")]
 
 /* Changelog:
    0.9.1+
