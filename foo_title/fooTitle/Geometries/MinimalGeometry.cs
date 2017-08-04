b6e92db0e2d8ef8002fc4b758433cdc5fc4368d2
TheQwertiest <qwertiest@mail.ru>
2017-08-04 14:48:50 +0300

    Added aligment attribute to minimal geometry

--- foo_title/fooTitle/Geometries/MinimalGeometry.cs
+++ foo_title/fooTitle/Geometries/MinimalGeometry.cs
@@ -6,6 +6,19 @@ namespace fooTitle.Geometries {
     [GeometryTypeAttribute("minimal")]
     internal class MinimalGeometry : Geometry{
 
+        [Flags]
+        public enum AlignType
+        {
+            None = 0,
+            Left = 1,
+            Right = 1 << 1,
+            Top = 1 << 2,
+            Bottom = 1 << 4,
+            Center = 1 << 5
+        };
+
+        private readonly Rectangle _parentRect;
+
         /// <summary>
         /// Current values of padding
         /// </summary>
@@ -18,7 +31,20 @@ namespace fooTitle.Geometries {
 
         private Size _curContentSize;
 
+        private AlignType _align;
+        public AlignType Align
+        {
+            get => _align;
+            set
+            {
+                _align = value;
+                Update(_parentRect);
+            }
+        }
+
         public MinimalGeometry(Rectangle parentRect, XmlNode node) : base(parentRect, node) {
+            _parentRect = parentRect;
+
             XmlNode padding = GetFirstChildByName(node, "padding");
 
             // read and store expressions
@@ -32,7 +58,31 @@ namespace fooTitle.Geometries {
             MyPadding.Top = (int)GetNumberFromAttribute(padding, "top", "0");
             MyPadding.Right = (int)GetNumberFromAttribute(padding, "right", "0");
             MyPadding.Bottom = (int)GetNumberFromAttribute(padding, "bottom", "0");
-            
+
+            // read alignment
+            string alignStr = GetAttributeValue(GetFirstChildByName(node, "position"), "align", "left,top");
+            _align = AlignType.None;
+            foreach (string i in alignStr.ToLower().Split(new char[] { ',', ' ' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                switch (i)
+                {
+                    case "top":
+                        _align |= AlignType.Top;
+                        break;
+                    case "bottom":
+                        _align |= AlignType.Bottom;
+                        break;
+                    case "right":
+                        _align |= AlignType.Right;
+                        break;
+                    case "left":
+                        _align |= AlignType.Left;
+                        break;
+                    case "center":
+                        _align |= AlignType.Center;
+                        break;
+                }
+            }
         }
 
         public override void Update(Rectangle parentRect) {
@@ -41,11 +91,38 @@ namespace fooTitle.Geometries {
             MyPadding.Right = GetValueFromExpression(MyExprPadding.Right, MyPadding.Right);
             MyPadding.Bottom = GetValueFromExpression(MyExprPadding.Bottom, MyPadding.Bottom);
 
-            myClientRect.X = MyPadding.Left + parentRect.Left;
-            myClientRect.Y = MyPadding.Top + parentRect.Top;
+            myClientRect.Width = Math.Max(0, Math.Min(_curContentSize.Width, parentRect.Width) - (MyPadding.Left + MyPadding.Right));
+            myClientRect.Height = Math.Max(0, Math.Min(_curContentSize.Height, parentRect.Height) - (MyPadding.Top + MyPadding.Bottom));
 
-            myClientRect.Width = Math.Min(_curContentSize.Width, parentRect.Width) - (MyPadding.Left + MyPadding.Right);
-            myClientRect.Height = Math.Min(_curContentSize.Height, parentRect.Height) - (MyPadding.Top + MyPadding.Bottom);
+            Point position = new Point(0,0);
+            if ((_align & AlignType.Left) != 0)
+            {
+                position.X = MyPadding.Left;
+            }
+            else if ((_align & AlignType.Right) != 0)
+            {
+                position.X = parentRect.Right - (myClientRect.Width + MyPadding.Right);
+            }
+            else
+            {
+                position.X = (parentRect.Right - (myClientRect.Width + MyPadding.Right - MyPadding.Left)) / 2;
+            }
+
+            if ((_align & AlignType.Top) != 0)
+            {
+                position.Y = MyPadding.Top;
+            }
+            else if ((_align & AlignType.Bottom) != 0)
+            {
+                position.Y = parentRect.Bottom - (myClientRect.Height + MyPadding.Bottom);
+            }
+            else
+            {
+                position.Y = (parentRect.Bottom - (myClientRect.Height + MyPadding.Bottom - MyPadding.Top))/2;
+            }
+
+            myClientRect.X = position.X + parentRect.Left;
+            myClientRect.Y = position.Y + parentRect.Top;
         }
 
         public override Size GetMinimalSize(Size contentSize)
@@ -55,8 +132,9 @@ namespace fooTitle.Geometries {
             return _curContentSize;
         }
 
-        public override Point GetPosition() {
-            return new Point(0, 0);
+        public override Point GetPosition()
+        {
+            return new Point(0,0);
         }
 
         public override bool IsDynamic() {
